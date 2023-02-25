e47a0f70836d3b181a810eb4826331b6cf7214b3
enferesa <enferesa@gmail.com>
2023-02-25 18:14:28 +0100

    Trabajo realizado

---
+++ main.js
@@ -0,0 +1,13 @@
+const numero = 1000;
+console.log ('numero')
+for (let lista =1; lista <=1000; lista ++){
+    if (lista %3 === 0 && lista %5 === 0){
+        console.log ('FizzBuzz')}
+    else if (lista %3 === 0){
+       console.log ('Fizz')}
+    else if (lista %5 === 0){
+       console.log ('Buzz')}
+    else 
+        console.log (lista)
+    
+}
